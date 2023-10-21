18919b978942960b69a18dfbc04072c2e42fe363
Fmquito45 <fmquitox45@gmail.com>
2023-10-21 18:28:53 -0400

    creates files for routes folder

--- models/inedx.js
+++ models/inedx.js
@@ -1,5 +1,5 @@
 const User = require("./User.js");
 const Reaction = require("./Reaction.js");
 const Thought = require("./Thought.js");
-
+// exports models user, reaction, and thought
 module.exports = { User, Reaction, Thought };
