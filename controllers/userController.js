33913bad32fb7e9c75a53e6ddb8f36379c622ea4
Fmquito45 <fmquitox45@gmail.com>
2023-10-21 18:57:29 -0400

    creates controller for user

---
+++ controllers/userController.js
@@ -0,0 +1,81 @@
+const { User } = require('../models');
+
+const UserController = {
+  // get all users
+  getAllUsers(req, res) {
+    User.find({})
+      .then(userData => res.json(userData))
+      .catch(err => res.status(500).json(err));
+  },
+  // get one user 
+  getUserById(req, res) {
+    User.findById(req.params.userId)
+      .then(userData => res.json(userData))
+      .catch(err => res.status(500).json(err));
+  },
+  // create a user
+  createUser(req, res) {
+    User.create(req.body)
+      .then(userData => res.json(userData))
+      .catch(err => res.status(500).json(err));
+  },
+  // update user by ID
+  updateUserById(req, res) {
+    User.findOneAndUpdate(req.params.id, req.body, { new: true })
+      .then(userData => {
+        if (!userData) {
+          return res.status(500).json({ message: 'User not found'});
+        }
+        res.json(userData);
+      })
+      .catch(err => res.status(500).json(err));
+  },
+  // delete user
+  deleteUserById(req, res) {
+    User.findOneAndDelete(req.params.id)
+      .then(userData => {
+        if (!userData) {
+          return res.status(500).json({ message: 'User not found'});
+        }
+        res.json({ message: 'User deleted' });
+      })
+      .catch(err => res.status(500).json(err));
+  },
+  // add friend
+  addFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.body.friendId || req.params.friendId} },
+      { new: true }
+    )
+      .then(userData => {
+        if (!userData) {
+          return res.status(500).json({ message: 'Friend not found'});
+        }
+        res.json(userData);
+      })
+      .catch(err => res.status(500).json(err));
+  },
+  // remove friend 
+  removeFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $pull: { friends: params.friendId } },
+      { new: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(500).json({ message: 'Friend not found'});
+        }
+        const removed = !dbUserData.friends.includes(params.friendId);
+        if (removed) {
+          res.json({ message: 'Friend removed', dbUserData});
+        } else {
+          res.json(dbUserData);
+        }
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+};
+// export UserController
+module.exports = UserController;
