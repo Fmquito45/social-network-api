6c0cf7a17d946a8ca9ab7d9d4a74950728d2ca48
Fmquito45 <fmquitox45@gmail.com>
2023-10-21 21:43:21 -0400

    creates controller for thoughts

---
+++ controllers/thoughtController.js
@@ -0,0 +1,89 @@
+const { Reaction, Thought, User } = require('../models');
+const mongoose = require('mongoose');
+
+const ThoughtController = {
+  async getThoughts(req, res) {
+    try {
+      const thoughts = await Thought.find({});
+      res.json(thoughts);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async getOneThought(req, res) {
+    try {
+      const thought = await Thought.findOne({_id:req.params.thoughtId});
+      if (!thought) {
+        res.status(500).json(err);
+      } else {
+        res.json(thought);
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async createThought(req, res) {
+    try {
+      const thought = await Thought.create(req.body);
+      res.status(200).json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  
+  async deleteThought(req,res) {
+    try {
+        const thought = await Thought.findByIdAndDelete({_id:req.params.thoughtId});
+        res.status(200).json(thought);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+  },
+
+  async updateThought(req, res) {
+    try {
+      const thought = await Thought.findByIdAndUpdate(req.params.thoughtId, req.body, {
+        new: true,
+      });
+      if (!thought) {
+        res.status(500).json(err);
+      } else {
+        res.json(thought);
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async createReaction(req, res) {
+      try {
+        const thought = await Thought.findOneAndUpdate(
+            {_id:req.params.thoughtId},
+            {$addToSet: {reactions: req.body}},
+            {runValidators: true, new: true}
+        );
+        thought ? res.json(thought) : res.status(500).json(err);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+  },
+
+  async deleteReaction(req, res) {
+      try {
+        const thought = await Thought.findOneAndUpdate(
+            {_id: req.params.thoughtId},
+            {$pull: {reactions: {reactionId: req.params.reactionId}}},
+            {runValidators: true, new: true}
+        );
+
+        thought ? res.json(thought) : res.status(500).json(err);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+  },
+
+};
+// Export ThoughtController
+module.exports = ThoughtController;
